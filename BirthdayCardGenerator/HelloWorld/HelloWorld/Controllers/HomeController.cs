d636a5c730ca86cb32924fb31ea9c9e5e73d89c4
GreenFish <sabia30@hotmail.com>
2020-05-31 21:11:38 -0700

    Birthday Card Generator

---
+++ BirthdayCardGenerator/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RsvpForm(Models.GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
---
+++ BirthdayCardGenerator/HelloWorld/HelloWorld/Models/GuestResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+namespace HelloWorld.Models
+{
+
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter whose birthday")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter your message")]
+        public string Message { get; set; }
+    
+    }
+}
